97b7b75f34083a6b7573a6d82aa5d86d62b847e0
rudy <rudy2022.163.com>
2022-06-19 12:58:20 +0800

    first commit

---
+++ src/main/java/com/rudy/gittest1.java
@@ -0,0 +1,14 @@
+package com.rudy;
+
+/**
+ * @Author : songyuan
+ * @Description :
+ * Date : 2022/6/19 12:56
+ * @Version : 1.0
+ */
+public class gittest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+    }
+}
