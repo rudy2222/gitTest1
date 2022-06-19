ccbc233356685e4dcf8eb110d5e27569a35d21cb
rudy <rudy2022.163.com>
2022-06-19 13:34:46 +0800

    hf2 commit

--- src/main/java/com/rudy/gittest1.java
+++ src/main/java/com/rudy/gittest1.java
@@ -12,6 +12,7 @@ public class gittest1 {
         System.out.println("hello git1");
         System.out.println("hello git1 hf1");
         System.out.println("hello git1 hf2");
+        System.out.println("hello git1 hf3");
 
     }
 }
